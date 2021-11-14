4890ebc600905f0a15e30cf04fe0b6d2339e0600
saba-kavtaradze-epam <87390407+saba-kavtaradze-epam@users.noreply.github.com>
2021-11-14 12:13:29 +0400

    new models

--- ERP/admin.py
+++ ERP/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from ERP.models import *
 
-# Register your models here.
+
+@admin.register(ImportedCars)
+class ImportedCarsAdmin(admin.ModelAdmin):
+    list_display = ['vin_code', 'import_date', 'model', 'make', 'color']
+
+
+@admin.register(Parts)
+class PartsAdmin(admin.ModelAdmin):
+    list_display = ['part_name', 'part_number']
+    search_fields = ['part_number']
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['car', 'part_name', 'part_number', 'barcode', 'selling_price', 'note']
+    autocomplete_fields = ['part_number']
+    search_fields = ['barcode']
+    # readonly_fields = ['part_name']
--- ERP/models.py
+++ ERP/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import random
 
-# Create your models here.
+
+class ImportedCars(models.Model):
+    vin_code = models.CharField(default=None, max_length=10)
+    import_date = models.DateField()
+    model = models.CharField(default=None, max_length=10)
+    make = models.CharField(default=None, max_length=10)
+    color = models.CharField(default=None, max_length=20, null=True)
+
+    class Meta:
+        verbose_name = 'ImportedCar'
+        verbose_name_plural = 'ImportedCars'
+
+    def __str__(self):
+        return self.vin_code
+
+
+class Parts(models.Model):
+    part_number = models.CharField(unique=True, max_length=150)
+    part_name = models.CharField(default=None, max_length=10)
+
+    class Meta:
+        verbose_name = 'Part'
+        verbose_name_plural = 'Parts'
+
+    def __str__(self):
+        return str(self.part_number)
+
+
+class Stock(models.Model):
+    car = models.ForeignKey(ImportedCars, default=None, on_delete=models.PROTECT)
+    part_name = models.CharField(default=None, max_length=100, blank=True)
+    part_number = models.ForeignKey(Parts, default=None, on_delete=models.PROTECT)
+    barcode = models.IntegerField(default=random.randint(111111111, 999999999), unique=True)
+    note = models.TextField(default=None)
+    selling_price = models.IntegerField(default=0)
+    amount_in_stock = models.IntegerField(default=1)
+
+
+@receiver(post_save, sender=Stock, dispatch_uid="update_part_number")
+def update_part_number(sender, **kwargs):
+    obj1 = Stock.objects.last()
+    part_number = getattr(obj1, 'part_number')
+    obj2 = Parts.objects.filter(part_number=part_number).values('part_name')
+    part_name_value = (obj2[0]['part_name'])
+    print(part_name_value)
+    x = Stock.objects.latest('id')
+    latest_id = x.id
+    Stock.objects.filter(id=latest_id).update(part_name=part_name_value)
+    pass
+
+
